bf1745a4b2c1683f7ff4310ceef0983aae98d78d
Anton Savoskin <asav@dvhb.ru>
2019-06-04 16:10:49 +0700

    fix: error message

--- src/utils.ts
+++ src/utils.ts
@@ -55,5 +55,5 @@ export const request = <T>({ url, body, qs, headers = {}, ...rest }: Options) =>
 };
 
 const format = (time: Date) => time.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
-export const showError = (message: string) => console.error(`\x1b[31m${message}\x1b[0m`);
+export const showError = (message: string) => console.error('\x1b[31m', message, '\x1b[0m');
 export const showInfo = (message: string) => console.info(`\x1b[34m[${format(new Date())}]\x1b[0m`, `${message}`);
